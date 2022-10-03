ea45efcddfc46e7d96211132c5a90fed1dd18808
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-10-03 22:44:15 +0900

    RegisterコントローラにAPIで接続した

--- resources/js/components/auth/Register.vue
+++ resources/js/components/auth/Register.vue
@@ -4,21 +4,21 @@
         <form>
             <div>
                 <label for="name">名前</label>
-                <input type="text" name="name" id="name" :v-model="name">
+                <input type="text" name="name" id="name" v-model="user.name">
             </div>
             <div>
                 <label for="email">メールアドレス</label>
-                <input type="email" name="email" id="email" :v-model="email">
+                <input type="email" name="email" id="email" v-model="user.email">
             </div>
             <div>
                 <label for="password">パスワード</label>
-                <input type="password" name="password" id="password" :v-model="password">
+                <input type="password" name="password" id="password" v-model="user.password">
             </div>
             <div>
                 <label for="password_confirmation">(確認用)パスワード</label>
-                <input type="password" name="password_confirmation" id="password_confirmation" :v-model="password_confirmation">
+                <input type="password" name="password_confirmation" id="password_confirmation" v-model="user.password_confirmation">
             </div>
-            <button type="submit">送信する</button>
+            <button @click="submit" :disabled="sending">送信する</button>
         </form>
     </div>
 </template>
@@ -26,10 +26,25 @@
 export default {
     data() {
         return {
-            name: null,
-            email: null,
-            password: null,
-            password_confirmation: null
+            user: {
+                name: null,
+                email: null,
+                password: null,
+                password_confirmation: null
+            },
+            sending: false
+        }
+    },
+    methods: {
+        async submit() {
+            this.sending = true;
+            try {
+                const response = await axios.post('/api/register', this.user);
+                console.log(response);
+            } catch (error) {
+                console.log(error);
+            }
+            this.sending = false;
         }
     }
 }
