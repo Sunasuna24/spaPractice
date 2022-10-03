c67fad8cb47690fc921ed45c124d1768d2ac4971
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-10-03 23:00:28 +0900

    ユーザーを作成しテーブルに格納した

--- app/Http/Controllers/Api/AuthenticationController.php
+++ app/Http/Controllers/Api/AuthenticationController.php
@@ -3,12 +3,26 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthenticationController extends Controller
 {
     public function register(Request $request)
     {
-        return response()->json();
+        $data = $request->validate([
+            'name' => ['required'],
+            'email' => ['required'],
+            'password' => ['required', 'confirmed']
+        ]);
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password'])
+        ]);
+
+        return $user;
     }
 }
